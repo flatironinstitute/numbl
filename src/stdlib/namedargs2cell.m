877e02d3eaa65847a0f304966ad9a776325df73f
Jeremy Magland <jmagland@flatironinstitute.org>
2026-03-11 19:47:15 -0400

    Make namedargs2cell a builtin instead of stdlib .m file
    Move namedargs2cell from a MATLAB stdlib file to a TypeScript builtin
    registered in misc.ts for better performance, since it is used in the
    function argument validation pipeline.
    
    Closes #3

--- src/stdlib/namedargs2cell.m
+++
@@ -1,9 +0,0 @@
-function C = namedargs2cell(S)
-    names = fieldnames(S);
-    n = numel(names);
-    C = cell(1, 2 * n);
-    for i = 1:n
-        C{2*i - 1} = names{i};
-        C{2*i} = S.(names{i});
-    end
-end
