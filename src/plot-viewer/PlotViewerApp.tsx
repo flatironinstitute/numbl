4f18b875ed0bf525deffe9e1edc0ed8f30b7311f
Jeremy Magland <jmagland@flatironinstitute.org>
2026-03-19 19:12:57 -0400

    formatting

--- src/plot-viewer/PlotViewerApp.tsx
+++ src/plot-viewer/PlotViewerApp.tsx
@@ -5,39 +5,7 @@ import {
   figuresReducer,
   initialFiguresState,
 } from "../graphics/figuresReducer.js";
-
-/** Restore NaN/Infinity values that were converted to null by JSON serialization. */
-function restoreNullsToNaN(arr: number[]): void {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === null) (arr as number[])[i] = NaN;
-  }
-}
-
-function restoreNaNs(instr: PlotInstruction): void {
-  if (instr.type === "plot") {
-    for (const t of instr.traces) {
-      restoreNullsToNaN(t.x);
-      restoreNullsToNaN(t.y);
-    }
-  } else if (instr.type === "plot3") {
-    for (const t of instr.traces) {
-      restoreNullsToNaN(t.x);
-      restoreNullsToNaN(t.y);
-      restoreNullsToNaN(t.z);
-    }
-  } else if (instr.type === "surf" || instr.type === "mesh") {
-    restoreNullsToNaN(instr.trace.x);
-    restoreNullsToNaN(instr.trace.y);
-    restoreNullsToNaN(instr.trace.z);
-    if (instr.trace.c) restoreNullsToNaN(instr.trace.c);
-  } else if (instr.type === "imagesc") {
-    restoreNullsToNaN(instr.trace.z);
-  } else if (instr.type === "contour") {
-    restoreNullsToNaN(instr.trace.x);
-    restoreNullsToNaN(instr.trace.y);
-    restoreNullsToNaN(instr.trace.z);
-  }
-}
+import { restoreNaNs } from "../graphics/restoreNaNs.js";
 
 export function PlotViewerApp() {
   const [figures, dispatch] = useReducer(figuresReducer, initialFiguresState);
